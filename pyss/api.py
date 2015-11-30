6f75d58c23630a0f0ca7945841e6f604ee1b7a36
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2015-11-30 13:57:24 +0100

    cli & api: add download command/function

--- pyss/api.py
+++ pyss/api.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from . import Core
+from .config import headers
 
 
-# TODO: download()
+def download(url, user_agent=headers['User-Agent']):
+    """Downloads stream."""
+    # TODO: ability to set quality, time
+    c = Core(user_agent)
+    streams = c.getManifest(url)
+    c.getStreams(streams)  # work in progress
+
+
 # todo: play?
--- pyss/cli.py
+++ pyss/cli.py
@@ -3,21 +3,30 @@
 """PySS's cli - simple script for managing Microsoft Smooth Streaming.
 
 Usage:
-    pyss <url> [--debug]
+    pyss (download | dl) <url> [--user-agent=<user-agent>] [--verbose] [--debug]
     pyss (-h | --help)
     pyss --version
 
 Options:
     -h --help     Show this screen.
     --version     Show version.
+    --verbose     Be more verbose
+    --debug       Debug (saves logs)
+    -ua <user-agent>, --user-agent=user-<agent>  User-Agent string.
 """
 from docopt import docopt
-from . import __version__
+from . import __version__, download
+from .config import headers
 
 
 def main():
+    # TODO: default user-agent string in docopt.
     args = docopt(__doc__, version=__version__)
+    if args['--user-agent'] == 'null':
+        args['--user-agent'] = headers['User-Agent']
     print(args)
+    if args['download'] or args['dl']:
+        download(args['<url>'], user_agent=args['--user-agent'], debug=args['--debug'])
 
 
 if __name__ == '__main__':
