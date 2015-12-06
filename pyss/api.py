84ce15c2eadb7a9334ba66925d0fae0ee7c9ed47
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2015-12-06 19:50:26 +0000

    api: download saves files, add ability to set destination

--- pyss/api.py
+++ pyss/api.py
@@ -4,12 +4,22 @@ from . import Core
 from .config import headers
 
 
-def download(url, user_agent=headers['User-Agent']):
+def download(url, dest='.', user_agent=headers['User-Agent']):
     """Downloads stream."""
     # TODO: ability to set quality, time
     c = Core(user_agent)
     streams = c.getManifest(url)
-    return c.getStreams(streams)  # returns generator
+    if c.live:
+        print 'Downloading live stream...'
+    else:
+        print 'Downloading [n] chunks...'
+    n = 1
+    for v, a in c.getStreams(streams):
+        with open('%s/video/%s' % (dest, n), 'wb') as fv:
+            fv.write(v)
+        with open('%s/audio/%s' % (dest, n), 'wb') as fa:
+            fa.write(a)
+        n += 1
 
 
 # todo: play?
